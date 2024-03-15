9b9eecffefd1bf444f2385f371f0ce4e1ca76018
IT21822230 <it21822230@my.sliit.lk>
2024-03-15 09:34:59 +0530

    Fix bug in login functionality

--- lab03/src/main/java/com/labsheet3/lab03/Lab03Application.java
+++ lab03/src/main/java/com/labsheet3/lab03/Lab03Application.java
@@ -1,13 +1,19 @@
-package com.labsheet3.lab03;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Lab03Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab03Application.class, args);
 	}
 
+	@GetMapping("/")
+	public String rootEndpoint() {
+		String message =  "Hello World!";
+		return message;
+	}
 }
